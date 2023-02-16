3f9c244a3d26611ad80bda15742b1af77d942792
Lucas Mas <lucas_mas@msn.com>
2023-02-16 02:56:08 +0100

    depth first graph browsing

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
 const canvas : HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
 const context : CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 const mousePosition : HTMLInputElement = document.getElementById("mouse-pos") as HTMLInputElement;
+const route: HTMLTextAreaElement = document.getElementById("route") as HTMLTextAreaElement;
+const nextButton: HTMLButtonElement = document.getElementById("next") as HTMLButtonElement;
 
 canvas.addEventListener('mousemove', (e) => {
     mousePosition.value = `x: ${e.clientX}, y: ${e.clientY}`
 })
 
-interface Vertice { x: number, y: number }
+interface Vertice { x: number, y: number, label: string }
 interface Vertices { [key: string] : Vertice }
+type Edge = Vertice[]
 
 const vertices: Vertices = { 
-    s: { x : 20, y: 20 },    
-    a: { x: 138, y: 122 },    
-    b: { x: 180, y: 267 },    
-    c: { x: 300, y: 300 },    
-    d: { x: 300, y: 60 },    
-    e: { x: 400, y: 150 },    
-    f: { x: 78, y: 383 },
-    g: { x: 350, y: 450 },
-    h: { x: 460, y: 290 },
-    i: { x: 30, y: 260}
+    s: { x : 20, y: 20, label: 's' },    
+    a: { x: 138, y: 122, label: 'a'},    
+    b: { x: 180, y: 267, label: 'b'},    
+    c: { x: 300, y: 300, label: 'c'},    
+    d: { x: 300, y: 60, label: 'd'},    
+    e: { x: 400, y: 150, label: 'e' },    
+    f: { x: 78, y: 383, label: 'f' },
+    g: { x: 350, y: 450, label: 'g' },
+    h: { x: 460, y: 290, label: 'h' },
+    i: { x: 30, y: 260, label: 'i'}
 }    
 
 const edges = [
@@ -39,50 +42,98 @@ const edges = [
     [vertices.i, vertices.b]
 ]    
 
+const drawEdge = () => {
+    for (let index = 0; index < edges.length; index++) {
+        const edge = edges[index];
+        context.lineWidth = 3;
+        context.beginPath()
+        context.moveTo(edge[0].x, edge[0].y)
+        context.lineTo(edge[1].x, edge[1].y)
+        context.stroke(); 
+    }
+}
+
+function drawVertices() {
+    for (const [label, vertice] of Object.entries(vertices)) {
+        context.font = "25px arial";
+        context.fillStyle = "black";
+        context.fillText(label, vertice.x + 1, vertice.y - 4);
+        context.beginPath();
+        context.moveTo(vertice.x, vertice.y);
+        context.arc(vertice.x, vertice.y, 3, 0, Math.PI * 2);
+        context.fill();
+    }
+}
+
+const colorEdge = (edge: Edge) => {
+    context.beginPath()
+    context.strokeStyle = "red"
+    context.lineWidth = 3;
+    context.moveTo(edge[0].x, edge[0].y)
+    context.lineTo(edge[1].x, edge[1].y)
+    context.stroke(); 
+}
+
+const uncolorEdge = (edge: Edge) => {
+    context.beginPath()
+    context.strokeStyle = "black";
+    context.lineWidth = 3;
+    context.moveTo(edge[0].x, edge[0].y)
+    context.lineTo(edge[1].x, edge[1].y)
+    context.stroke();  
+}
+
+const depthFirst = (start: Vertice, end: Vertice) => {
+    let step = 0;
+    const edgeQueue : Edge[] = [];
+    const edgeDone : Edge[] = [];
+    let nextVertice = start    
+    let nextEdge: Vertice[] = [];
+    route.value = nextVertice.label;
+
+    const browseGraph = () => {
+        let nextEdges = edges.filter(edge => {
+            return edge.includes(nextVertice) && !edgeDone.includes(edge)
+        });
+        if(nextEdges.length == 0) {
+            nextVertice = edgeDone[step-1].find(vertice => vertice != nextVertice) as Vertice;
+            uncolorEdge(edgeDone[step-1])
+            step -= 1;
+        } else {
+            step += 1;
+            edgeQueue.unshift(...nextEdges)
+            nextEdge = edgeQueue.shift() as Vertice[];
+            edgeDone.push(nextEdge)
+            colorEdge(nextEdge);
+            nextVertice = nextEdge.find(vertice => vertice != nextVertice) as Vertice
+            route.value += ` -> ${nextVertice.label}`
+            if(nextVertice == end) {
+                context.font = "50px arial"
+                context.fillStyle = "green"
+                context.textAlign = "center"
+                context.fillText("FOUND", canvas.height / 2, canvas.width / 2)
+                nextButton.removeEventListener("click", browseGraph);
+            }
+        }
+    }
+
+    nextButton.addEventListener("click", browseGraph)
+}
 
 const draw = () => {
     context.clearRect(0,0, canvas.width, canvas.height);
     context.strokeRect(0, 0, canvas.width, canvas.height);
     context.save();
 
-    for (const [key, value] of Object.entries(vertices)) {
-        context.font = "25px serif";
-        context.fillStyle = "black";
-        context.fillText(key, value.x +1 , value.y - 4 );
-        context.beginPath();
-        context.moveTo(value.x, value.y);
-        context.arc(value.x, value.y, 3, 0, Math.PI * 2)
-        context.fill()
-   }
-
-    for (let index = 0; index < edges.length; index++) {
-        const edge = edges[index];
-        context.beginPath()
-        context.moveTo(edge[0].x, edge[0].y)
-        context.lineTo(edge[1].x, edge[1].y)
-        context.stroke(); 
-    }
+    drawVertices();
+    drawEdge();
+    depthFirst(vertices.s, vertices.g);
     
     context.restore(); 
 }
-
 draw()
 // window.requestAnimationFrame(draw)
 
-// const depthFirst = (start: Vertice, end: Vertice) => {
-//    const queue = [];
-//    const startEdges = edges.filter(edge => edge.includes(start));
-   
-//    queue.push(...startEdges)
-//    let nextEdge = queue.pop();
-//    let nextVertice = nextEdge?.find(edge => edge != start)
-//    while(nextVertice && nextVertice != end) {
-    
-//    }
-//    console.log(queue)
-// //    queue.push(...startEdges)
-// }
-// depthFirst(vertices.s, vertices.h)
 
 const breadthFirst = () => {
     //todo
